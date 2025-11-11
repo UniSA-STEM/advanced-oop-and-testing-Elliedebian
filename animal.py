27e74d13b2adccc87b54616c8ecb795b1f5f94ce
elliedebian <ellie.debian@icloud.com>
2025-11-11 19:44:18 +1030

    Completed header

--- animal.py
+++ animal.py
@@ -1,8 +1,9 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: animal.py
+Description: Defines the aminal hierarchy and basic health tracking for the zoo management system.
+Author: Ellie Debian
+ID: 110444485
+Username: debey003
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
