22be4ff139b7323323aa3855dfb86fec7e405b83
elliedebian <ellie.debian@icloud.com>
2025-11-11 19:56:19 +1030

    Add animal class with core attributes and data validation

--- animal.py
+++ animal.py
@@ -7,3 +7,20 @@ Username: debey003
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+from datetime import date
+
+"""Base class for all animals in the zoo"""
+class Animal:
+    def __init__(self, name: str, species: str, age: int, diet: str, environment_type: str) -> None:
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+        self.__environment_type = environment_type
+        self.__health_records = []
+
+        self.set_name(name)
+        self.set_species(species)
+        self.set_age(age)
+        self.set_diet(diet)
+        self.set_environment_type(environment_type)
